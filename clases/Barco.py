f88e76923bfb71a4cbf6a1c5ee852401362b2cc4
Nachosanchezz <isanccal@myuax.com>
2023-03-22 15:28:30 +0000

    Cambio

--- clases/Barco.py
+++ clases/Barco.py
@@ -1,5 +1,5 @@
 from venv import juego
-from clases import*
+from clases import *
 from clases import Conventions
 from clases import Case
 from itertools import product, repeat
@@ -12,38 +12,48 @@ instances = []
 casillas_ocupadas = set()
 
 class Barco:
+    def __init__(self, longitud, orientacion=choice(ORIENTACIONES), tocado=False, hundido=False):
+        self.longitud = longitud
+        self.orientacion = choice(ORIENTACIONES)
+        self.tocado = False
+        self.hundido = False
+
 
 # performance / legibilidad:
-num_lineas = Conventions.tablero_num_lineas
-num_columnas = Conventions.tablero_num_columnas
-num2l = Conventions.generar_num_linea
-num2c = Conventions.generar_num_columna
+        num_lineas = Conventions.tablero_num_lineas
+        num_columnas = Conventions.tablero_num_columnas
+        num2l = Conventions.generar_num_linea
+        num2c = Conventions.generar_num_columna
 
-
-def __init__(self, longitud, orientacion=choice(ORIENTACIONES), tocado=False, hundido=False):
-    self.longitud = longitud
-    self.orientacion = choice(ORIENTACIONES)
-    self.tocado = False
-    self.hundido = False
-
-def horizontal(self):
-    if self.orientacion == HORIZONTAL:
-        rang = choice(range(num_lineas))
-        primero = choice(range(num_columnas + 1 - LONGITUDES_BARCOS))
-        letra = num2l(rang)
-        cifras = [num2c(x) for x in range(primero, primero + LONGITUDES_BARCOS)]
-        self.casillas = {Case.instances[l + c]
-                             for l, c in product(repeat(letra, LONGITUDES_BARCOS), cifras)}
-    else:
-        rang = choice(range(num_columnas))
-        primero = choice(range(num_lineas + 1 - LONGITUDES_BARCOS))
-        cifra = num2c(rang)
-        letras = [num2l(x) for x in range(primero, primero + LONGITUDES_BARCOS)]
+        if self.orientacion == HORIZONTAL:
+            rang = choice(range(num_lineas))
+            primero = choice(range(num_columnas + 1 - longitud))
+            letra = num2l(rang)
+            cifras = [num2c(x) for x in range(primero, primero + longitud)]
+            self.casillas = {Case.instances[l + c]
+                             for l, c in product(repeat(letra, longitud), cifras)}
+        else:
+            rang = choice(range(num_columnas))
+            primero = choice(range(num_lineas + 1 - longitud))
+            cifra = num2c(rang)
+            letras = [num2l(x) for x in range(primero, primero + longitud)]
 
         # Crear el barco
-        self.casillas = {Case.instances[l + c]
-            for l, c in product(letras, repeat(cifra, LONGITUDES_BARCOS))}
-    return self.casillas
+            self.casillas = {Case.instances[l + c]
+                for l, c in product(letras, repeat(cifra, longitud))}
+        
+        for existente in instances:
+            if self.casillas.intersection(existente.casillas):
+                break
+            else:
+                instances.append(self)
+                for casilla in self.casillas:
+                    casilla.barco = self
+                casillas_ocupadas = self.casillas
+                break
+        else:
+            raise ValueError("No se pudo crear el barco")
+        
 
 
 def instanciar(self):
